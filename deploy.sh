5440370aa6e50762354991fce7bf54ea34868cce
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-07-19 14:58:38 +0530

    release version 0.3

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
-# Source: https://raw.github.com/thenbrent/multisite-user-management/master/deploy.sh
-# http://thereforei.am/2011/04/21/git-to-svn-automated-wordpress-plugin-deployment/
 # A modification of Dean Clatworthy's deploy script as found here: https://github.com/deanc/wordpress-plugin-git-svn
 # The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
 
@@ -14,40 +12,50 @@ GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
-SVNURL="http://plugins.svn.wordpress.org/$PLUGINSLUG" # Remote SVN repo on wordpress.org, with no trailing slash
-SVNUSER="ravinderk" # your svn username
+SVNURL="http://plugins.svn.wordpress.org/$PLUGINSLUG/" # Remote SVN repo on wordpress.org, with no trailing slash
+SVNUSER="Kloon" # your svn username
 
 
 # Let's begin...
 echo ".........................................."
 echo
-echo "Preparing to deploy WordPress plugin"
+echo "Preparing to deploy wordpress plugin"
 echo
 echo ".........................................."
 echo
 
-# Check version in readme.txt is the same as plugin file
-NEWVERSION1=`grep "^Stable tag" $GITPATH/readme.txt | awk -F' ' '{print $3}' | sed 's/[[:space:]]//g'`
-echo "readme version: $NEWVERSION1"
-NEWVERSION2=`grep "^Version" $GITPATH/$MAINFILE | awk -F' ' '{print $2}' | sed 's/[[:space:]]//g'`
+# Check version in readme.txt is the same as plugin file after translating both to unix line breaks to work around grep's failure to identify mac line breaks
+NEWVERSION1=`grep "^Stable tag:" $GITPATH/readme.txt | awk -F' ' '{print $NF}'`
+echo "readme.txt version: $NEWVERSION1"
+echo "$GITPATH$MAINFILE"
+NEWVERSION2=`grep "Version:" $GITPATH$MAINFILE | awk -F' ' '{print $NF}'`
 echo "$MAINFILE version: $NEWVERSION2"
 
-if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Versions don't match. Exiting...."; exit 1; fi
+if [ "$NEWVERSION1" -ne "$NEWVERSION2" ]; then echo "Version in readme.txt & $MAINFILE don't match. Exiting...."; exit 1; fi
 
-echo "Versions match in readme.txt and PHP file. Let's proceed..."
+echo "Versions match in readme.txt and $MAINFILE. Let's proceed..."
+
+if git show-ref --tags --quiet --verify -- "refs/tags/$NEWVERSION1"
+    then
+		echo "Version $NEWVERSION1 already exists as git tag. Exiting....";
+		exit 1;
+	else
+		echo "Git version does not exist. Let's proceed..."
+fi
 
 cd $GITPATH
 echo -e "Enter a commit message for this new version: \c"
 read COMMITMSG
-# git commit -am "$COMMITMSG"
+git commit -am "$COMMITMSG"
 
-# echo "Tagging new version in git"
-# git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
+echo "Tagging new version in git"
+git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
 
-# echo "Pushing latest commit to origin, with tags"
-# git push origin master
-# git push origin master --tags
+echo "Pushing latest commit to origin, with tags"
+git push origin master
+git push origin master --tags
 
+echo
 echo "Creating local copy of SVN repo ..."
 svn co $SVNURL $SVNPATH
 
@@ -66,11 +74,11 @@ cd $SVNPATH/trunk/
 svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
 svn commit --username=$SVNUSER -m "$COMMITMSG"
 
-# echo "Creating new SVN tag & committing it"
+echo "Creating new SVN tag & committing it"
 cd $SVNPATH
-svn copy trunk tags/$NEWVERSION1
+svn copy trunk/ tags/$NEWVERSION1/
 cd $SVNPATH/tags/$NEWVERSION1
-svn commit --username=$SVNUSER -m "Tag $NEWVERSION1"
+svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
 
 echo "Removing temporary directory $SVNPATH"
 rm -fr $SVNPATH/
